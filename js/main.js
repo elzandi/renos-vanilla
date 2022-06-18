6955b34c8a6c0f10d628c7baac50fa877f9c0c59
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2022-06-18 18:58:49 +0700

    feat: add modal

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    "use strict";
+
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("./sw.js");
+    }
+};
+
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+    trigger.addEventListener("click", function (event) {
+        event.preventDefault();
+        const modal = document.getElementById(trigger.dataset.modal);
+        modal.classList.add("open");
+        const exits = modal.querySelectorAll(".modal-exit");
+        exits.forEach(function (exit) {
+            exit.addEventListener("click", function (event) {
+                event.preventDefault();
+                modal.classList.remove("open");
+            });
+        });
+    });
+    trigger.addEventListener("keydown", function (event) {
+        event.preventDefault();
+        const keyCode = event.keyCode;
+        if (keyCode === 27) {
+            const modal = document.getElementById(trigger.dataset.modal);
+            modal.classList.remove("open");
+        }
+    });
+});
